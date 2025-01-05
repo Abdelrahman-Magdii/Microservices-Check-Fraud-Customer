4b771ec2bd12913d25758d658b21589729fd2d76
Abdelrahman-Magdii <abdulrahman403828@fci.bu.edu.eg>
2025-01-05 15:36:20 +0200

    client services

--- clients/src/main/java/com/microservices/clients/fraud/FraudClient.java
+++ clients/src/main/java/com/microservices/clients/fraud/FraudClient.java
@@ -1,5 +1,9 @@
 package com.microservices.clients.fraud;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @FeignClient(
         name = "fraud",
         url = "${clients.fraud.url}"
